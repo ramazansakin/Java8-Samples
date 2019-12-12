8e34f63e420d85bab46579d3433c10b734588f3c
Ramazan Sakin <ramazansakin63@gmail.com>
2019-12-12 20:53:03 +0300

    Another sample usages on Java 8 - Stream

--- Sample-Stream-usage-pro/src/com/sakinramazan/java8/Main.java
+++ Sample-Stream-usage-pro/src/com/sakinramazan/java8/Main.java
@@ -1,8 +1,8 @@
 package com.sakinramazan.java8;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.math.BigDecimal;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -13,7 +13,8 @@ public class Main {
         List<String> lines = Arrays.asList("sample", "code", "java", "7");
         String filter = "code";
 
-
+        System.out.println("----------------------------------------------------------------------------------------");
+        System.out.println("Sample usage with Java7");
         // result list with filter - without "code" string
         List<String> result1 = getFilterOutput(lines, filter);
         // classic foreach to trace the list
@@ -21,6 +22,9 @@ public class Main {
             System.out.println(temp);
         }
 
+
+        System.out.println("----------------------------------------------------------------------------------------");
+        System.out.println("With Java8");
         // After Jdk-8
         List<String> result2 = getFilterOutputWithJava8(lines, filter);    // we dont have to write method indeed :)
         // just be able to compare them
@@ -28,7 +32,8 @@ public class Main {
 
 
         // -------------------------------------------------------------------------------------------------------------
-        // Sample 2
+        System.out.println("----------------------------------------------------------------------------------------");
+        System.out.println("Sample usage with Java8 - 2");
         List<Person> people = Arrays.asList(
                 new Person("Jack", "X", 30),
                 new Person("John", "Y", 20),
@@ -46,7 +51,9 @@ public class Main {
         res = getPersonByNameWithJava8(people, filter);
 
 
-        // another sample usages
+        // -------------------------------------------------------------------------------------------------------------
+        System.out.println("----------------------------------------------------------------------------------------");
+        System.out.println("Sample usage with Java8 - 3");
         // we can filter by any of the fields we want on person
         String tempFilter = "John";
         Person person1 = people.stream()
@@ -57,14 +64,116 @@ public class Main {
         System.out.println("Filtered Person 1 :" + person1);
 
         Person person2 = people.stream().filter(p -> {
-                    if (tempFilter.equals(p.getName()) && 20 == p.getAge()) {
-                        return true;
-                    }
-                    return false;
-                }).findAny()
+            if (tempFilter.equals(p.getName()) && 20 == p.getAge()) {
+                return true;
+            }
+            return false;
+        }).findAny()
                 .orElse(null);
 
         System.out.println("Filtered Person 2 :" + person2);
+
+
+        // -------------------------------------------------------------------------------------------------------------
+        System.out.println("----------------------------------------------------------------------------------------");
+        System.out.println("Sample usage with Java8 - 4");
+        List<String> chars = Arrays.asList("x", "y", "z", "t", "q");
+
+        // Java7-
+        List<String> uppers = new ArrayList<>();
+        for (String s : chars) {
+            uppers.add(s.toUpperCase());
+        }
+
+        System.out.println(chars);
+        System.out.println(uppers);
+
+        // Java8+
+        List<String> resultList = chars.stream().map(String::toUpperCase/* Method reference usage again*/)
+                .collect(Collectors.toList());
+        System.out.println(resultList); //[A, B, C, D]
+
+        // -------------------------------------------------------------------------------------------------------------
+        System.out.println("----------------------------------------------------------------------------------------");
+        System.out.println("Sample usage with Java8 - 5");
+        // Another basic map usage
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        List<Integer> collect1 = numbers.stream().map(n -> n * 2).collect(Collectors.toList());
+        System.out.println(collect1);
+
+
+        // -------------------------------------------------------------------------------------------------------------
+        // Another map example
+        List<Person> people2 = Arrays.asList(
+                new Person("Joe", "X", 30),
+                new Person("Jimmy", "Y", 20),
+                new Person("Kane", "Z", 45)
+        );
+
+        // Java7-
+        List<String> result = new ArrayList<>();
+        for (Person x : people2) {
+            result.add(x.getName());
+        }
+        System.out.println(result);
+
+        // Java8+
+        List<String> collect = people2.stream().map(Person::getName).collect(Collectors.toList());
+        System.out.println(collect);
+
+        List<String> items =
+                Arrays.asList("group1", "group2", "group1",
+                        "group2", "group2", "group1", "group3", "group1", "group3");
+
+        // -------------------------------------------------------------------------------------------------------------
+        // -------------------------------------------------------------------------------------------------------------
+        System.out.println("----------------------------------------------------------------------------------------");
+        System.out.println("Sample usage with Java8 - 6");
+        // Another stream usage - group by, sort and count
+        Map<String, Long> resultMap =
+                items.stream().collect(
+                        Collectors.groupingBy(
+                                Function.identity(), Collectors.counting()
+                        )
+                );
+
+        // grouping by result
+        System.out.println(resultMap);
+
+        Map<String, Long> finalMap = new LinkedHashMap<>();
+        // map sorting
+        resultMap.entrySet().stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue()
+                        .reversed()).forEachOrdered(e -> finalMap.put(e.getKey(), e.getValue()));
+
+        System.out.println(finalMap);
+
+
+        // -------------------------------------------------------------------------------------------------------------
+        // -------------------------------------------------------------------------------------------------------------
+        System.out.println("----------------------------------------------------------------------------------------");
+        System.out.println("Sample usage with Java8 - 7");
+        // static local datas to test grouping with map
+        List<Fruit> fruitList = Arrays.asList(
+                new Fruit("banana", 20, new BigDecimal("19.99")),
+                new Fruit("watermelon", 10, new BigDecimal("29.99")),
+                new Fruit("papaya", 20, new BigDecimal("9.99")),
+                new Fruit("banana", 10, new BigDecimal("19.99")),
+                new Fruit("orang", 10, new BigDecimal("29.99")),
+                new Fruit("watermelon", 10, new BigDecimal("29.99")),
+                new Fruit("papaya", 20, new BigDecimal("9.99")),
+                new Fruit("banana", 10, new BigDecimal("19.99")),
+                new Fruit("apple", 10, new BigDecimal("9.99")),
+                new Fruit("apple", 10, new BigDecimal("9.99")),
+                new Fruit("apple", 20, new BigDecimal("9.99"))
+        );
+
+
+        Map<String, Long> counting = fruitList.stream().collect(Collectors.groupingBy(Fruit::getName, Collectors.counting()));
+        System.out.println(counting);
+
+        Map<String, Integer> sum = fruitList.stream().collect(Collectors.groupingBy(Fruit::getName, Collectors.summingInt(Fruit::getQty)));
+        System.out.println(sum);
 
     }
 

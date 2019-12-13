02e4220b317e7da9310f801368d1b38f74939b18
Ramazan Sakin <ramazansakin63@gmail.com>
2019-12-13 22:33:46 +0300

    Sample Spring project with Hibernate CRUD operations

---
+++ spring-boot-hibernate-crud-demo/target/classes/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO 
+	TBL_EMPLOYEES (first_name, last_name, email) 
+VALUES
+  	('Ali', 'X', 'ali@gmail.com'),
+  	('Veli', 'Y', 'veli@email.com'),
+  	('Mustafa', 'Z', 'mustafa@gmail.com'),
+  	('Kaan', 'T', 'kaan@email.com');
